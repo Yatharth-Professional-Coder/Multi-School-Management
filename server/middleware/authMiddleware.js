265dc04e47c50ed46fe6cd39a97cfda265e1934b
Yatharth <yatharth@Yatharths-MacBook-Air.local>
2026-02-12 21:33:59 +0530

    Fix Middleware: allow SuperAdmin to access Admin routes

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -30,7 +30,7 @@ const protect = async (req, res, next) => {
 };
 
 const admin = (req, res, next) => {
-    if (req.user && req.user.role === 'Admin') {
+    if (req.user && (req.user.role === 'Admin' || req.user.role === 'SuperAdmin')) {
         next();
     } else {
         res.status(401);
