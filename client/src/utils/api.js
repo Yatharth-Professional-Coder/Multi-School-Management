3ccbfa0dbe70e725367b28229d7ae7482c80fcb1
Yatharth <yatharth@Yatharths-MacBook-Air.local>
2026-02-16 15:22:53 +0530

    debug: add diagnostic logs for authorization token tracking

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -17,7 +17,12 @@ api.interceptors.request.use(
             const user = JSON.parse(userStr);
             if (user && user.token) {
                 config.headers.Authorization = `Bearer ${user.token}`;
+                console.log('API Request: Token attached to', config.url);
+            } else {
+                console.log('API Request: No token found in user object');
             }
+        } else {
+            console.log('API Request: No user found in localStorage');
         }
         return config;
     },
--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -4,6 +4,8 @@ const User = require('../models/User');
 const protect = async (req, res, next) => {
     let token;
 
+    console.log('Authorization Header:', req.headers.authorization);
+
     if (
         req.headers.authorization &&
         req.headers.authorization.startsWith('Bearer')
